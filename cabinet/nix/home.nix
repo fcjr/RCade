3849fcd2f6fa7db16becb333c1dc41216beeba00
Rose Hall <rose@hall.ly>
2026-01-22 21:39:36 -0500

    nix :)

---
+++ cabinet/nix/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should manage
+  home.username = "rcade";
+  home.homeDirectory = "/home/rcade";
+
+  # Packages to install for this user
+  home.packages = with pkgs; [
+    # Rust toolchain
+    rustup
+    cargo
+    rustc
+    rust-analyzer
+    clippy
+    rustfmt
+
+    # C/C++ for Rust FFI and system programming
+    gcc
+    clang
+    llvm
+    cmake
+    pkg-config
+  
+    # Development tools
+    git
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "RCade";
+    userEmail = "rose@hall.ly";
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+    };
+  };
+
+  # Let Home Manager manage itself
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is compatible with
+  home.stateVersion = "24.05";
+}
